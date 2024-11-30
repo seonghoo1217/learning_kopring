8647ab74d846716718853b4934e0267b04a4a999
seonghoo1217 <lsh6451217@gmail.com>
2024-11-30 16:23:34 +0900

    Kopring initialize

---
+++ src/test/kotlin/learning/kopring/KopringApplicationTests.kt
@@ -0,0 +1,13 @@
+package learning.kopring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KopringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
